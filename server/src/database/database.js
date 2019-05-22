1fe4ad65b1407902ec764b6e49252388f898032a
lw <root@fsoc.space>
2019-05-22 16:20:59 +0200

    Move database related files to src/database/

---
+++ server/src/database/database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Vote } = require('./models');
+const { debug } = require('../server');
+const config = require('../../.config');
+
+const { MONGODB_NAME, MONGODB_PASSWORD } = process.env;
+const uri = `mongodb+srv://${MONGODB_NAME}:${MONGODB_PASSWORD}@`
+    + config.mongoDb.connectString;
+
+exports.connect = mongoose.connect.bind(this, uri, { useNewUrlParser: true });
+
+const addVote = exports.addVote = async (vote) => {
+    vote.creationDate = vote.creationDate || new Date(Date.now());
+
+    await new Vote(vote).save();
+};
+
+const getVotes = exports.getVotes = async () => {
+    return await Vote.find();
+}
