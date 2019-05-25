53f39c3c3792988cb728286bcfcae23b5d1160ba
lw <root@fsoc.space>
2019-05-25 14:09:15 +0200

    Show votes components

--- app/src/components/home/Home.jsx
+++ app/src/components/home/Home.jsx
@@ -3,6 +3,9 @@ import { withAuth } from '@okta/okta-react';
 
 import { Button, Header } from 'semantic-ui-react';
 
+import VotesList from '../votes/VotesList';
+import AddNewVote from '../votes/AddNewVote';
+
 import { checkAuthentication } from '../../helpers';
 
 export default withAuth(class Home extends Component {
@@ -35,17 +38,21 @@ export default withAuth(class Home extends Component {
              <Header as="h1"><i>vot</i>sec index</Header>
 
             <div>
-                {this.state.authenticated ? (<div>
-                    <p>Welcome back, {this.state.userinfo.email}!</p>
-                </div>) : (<div>
-                    <p>
-                        Optimally there'll be some text telling you more about this 
-                        project in the future.
-                    </p>
+                {this.state.authenticated ? (
+					<div>
+						<p>Welcome back, {this.state.user.email}!</p>
 
-                    <Button id="login-button" primary onClick={this.login}>
-                        Login</Button>
-                </div>)}
+						<AddNewVote />
+						<VotesList />
+					</div>
+				) : (
+					<div>
+						<VotesList />
+
+						<Button id="login-button" primary onClick={this.login}>
+							Login</Button>
+					</div>
+				)}
             </div>
         </div>);
     }
