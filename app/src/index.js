61a786f6b753f28165547f7223f650f26d08f887
lw <root@fsoc.space>
2019-05-26 18:30:07 +0200

    Remove dependency on obsolete CSS

--- app/src/index.js
+++ app/src/index.js
@@ -5,7 +5,6 @@ import App from './components/app/App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
