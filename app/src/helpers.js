1d2a491a643ac05d48d19a49961e2a3db83f000f
lw <root@fsoc.space>
2019-05-30 18:11:53 +0200

    Add isAdmin field to state

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -1,11 +1,14 @@
 async function checkAuthentication() {
-    const authenticated = await this.props.auth.isAuthenticated();
-
+	const authenticated = await this.props.auth.isAuthenticated();
+	
     if (this.state.authenticated !== authenticated) {
         if (authenticated && !this.state.user) {
+			const user = await this.props.auth.getUser();
+
             this.setState({
-                authenticated,
-                user: await this.props.auth.getUser()
+				user,
+				authenticated,
+				isAdmin: user.groups.includes('Admin'),
             });
         } else {
             this.setState({ authenticated });
