4f461e907aca8a9ed563412097873f132a5ac17f
lw <root@fsoc.space>
2019-06-03 12:02:59 +0200

    Add consistent spaces

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -1,19 +1,19 @@
 async function checkAuthentication() {
 	const authenticated = await this.props.auth.isAuthenticated();
-	
-    if (this.state.authenticated !== authenticated) {
-        if (authenticated && !this.state.user) {
+
+	if (this.state.authenticated !== authenticated) {
+		if (authenticated && !this.state.user) {
 			const user = await this.props.auth.getUser();
 
-            this.setState({
+			this.setState({
 				user,
 				authenticated,
 				isAdmin: user.groups.includes('Admin'),
-            });
-        } else {
-            this.setState({ authenticated });
-        }
-    }
+			});
+		} else {
+			this.setState({ authenticated });
+		}
+	}
 }
 
 export { checkAuthentication };
