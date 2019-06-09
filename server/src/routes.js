4b357dca38994e3f47ddc4b2fbe642921ff825fe
lw <root@fsoc.space>
2019-06-09 18:46:07 +0200

    Don't expose too much information in routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,10 @@
 module.exports = (fastify, opts, next) => {
 	const routes = [];
 
-	fastify.addHook('onRoute', (route) => routes.push(route));
+	fastify.addHook('onRoute', (route) => routes.push({
+		method: route.method,
+		path: route.path.
+	}));
 
 	fastify.get('/', async (request, reply) => {
 		return { message: 'Did you get lost?', routes };
