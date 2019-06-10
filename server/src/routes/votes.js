4ed366023702c500f2f158b89aa94bb4d68fd9bb
lw <root@fsoc.space>
2019-06-10 18:17:53 +0200

    Remove debug log

--- server/src/routes/votes.js
+++ server/src/routes/votes.js
@@ -1,8 +1,6 @@
 const { requireAuth } = require('../auth');
 
 module.exports = (fastify, opts, next) => {
-	fastify.log.trace(fastify.database);
-
 	fastify.get('/get', async (request, reply) => {
 		return await fastify.database.votes.get() || [];
 	});
