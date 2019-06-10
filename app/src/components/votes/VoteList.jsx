b5feff5c917358cd484083d0a030cb909d29cc03
lw <root@fsoc.space>
2019-06-10 18:17:13 +0200

    Refactor item design

--- app/src/components/votes/VoteList.jsx
+++ app/src/components/votes/VoteList.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 
-import { Item, Message } from 'semantic-ui-react';
+import { Item, Message, Divider, Icon, List, Grid, Header, Segment } from 'semantic-ui-react';
 
 import { config } from '../app/App';
+import { formatServerError } from '../../helpers'
 
 export default class VoteList extends Component {
 	constructor(props) {
@@ -27,13 +28,16 @@ export default class VoteList extends Component {
 
 	async getVotes() {
 		const { url, port } = config.resourceServer;
-		const res = await axios.get(`${url}:${port}/votes/get`);
 
-		if (res.status !== 200) {
-			return this.setState({ error: 'Failed getting votes: ' + res.statusText });
-		} else this.setState({ error: null });
+		try {
+			const res = await axios.get(`${url}:${port}/votes/get`);
 
-		this.setState({ votes: res.data });
+			this.setState({ votes: res.data, error: null });
+		} catch (err) {
+			console.error('getVotes', err);
+
+			this.setState({ error: formatServerError(err) });
+		}
 	}
 
 	handleMessageDismiss = () => {
@@ -48,11 +52,13 @@ export default class VoteList extends Component {
 					onDismiss={this.handleMessageDismiss}
 				/>
 
-				<Item.Group divided relaxed>
+				{/* <Item.Group divided> */}
 					{this.state.votes.map((vote) => (
 						<VoteItem vote={vote} key={vote._id} />
 					))}
-				</Item.Group>
+				{/* </Item.Group> */}
+
+				<Divider />
 			</React.Fragment>
 		);
 	}
@@ -70,13 +76,40 @@ class VoteItem extends Component {
 		const endDate = moment(this.vote.endDate).fromNow();
 
 		return (
-			<Item>
-				<Item.Content>
-					<Item.Header as='a'>{this.vote.title}</Item.Header>
-					<Item.Meta>Started {startDate}, ending {endDate}</Item.Meta>
-					<Item.Description>{this.vote.description}</Item.Description>
-				</Item.Content>
-			</Item>
+			// <Item>
+			// 	<Item.Content>
+			// 		<Item.Header as='a'>{this.vote.title}</Item.Header>
+			// 		<Item.Meta>Started {startDate}, ending {endDate}</Item.Meta>
+			// 		<Item.Description>
+			// 			{this.vote.description}
+			// 		</Item.Description>
+			// 	</Item.Content>
+			// </Item>
+
+			<Segment basic>
+				<Grid>
+					<Grid.Column width={1}>
+						<List>
+							<Item><Icon circular link name='arrow up' color='green' /></Item>
+							<Item><Icon circular link name='arrow down' color='red' /></Item>
+						</List>
+					</Grid.Column>
+
+					<Grid.Column width={15}>
+						<Header as='h2'>
+							{this.vote.title}
+
+							<Header.Subheader>
+								Started {startDate}, ending {endDate}
+							</Header.Subheader>
+						</Header>
+
+						<p>
+							{this.vote.description}
+						</p>
+					</Grid.Column>
+				</Grid>
+			</Segment>
 		);
 	}
 }
