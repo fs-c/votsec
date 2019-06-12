63e73819af2881b9fa7067febcd03aee782b34e3
lw <root@fsoc.space>
2019-06-12 15:21:05 +0200

    Remove authentication logic from Home

--- app/src/components/home/Home.jsx
+++ app/src/components/home/Home.jsx
@@ -1,43 +1,23 @@
 import React, { Component } from 'react';
-import { withAuth } from '@okta/okta-react';
 
 import { Divider } from 'semantic-ui-react';
 
 import VoteList from '../votes/VoteList';
 import AddNewVote from '../votes/AddNewVote';
 
-import { checkAuthentication } from '../../helpers';
-
-export default withAuth(class Home extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { authenticated: null, user: null, isAdmin: false };
-
-        this.checkAuthentication = checkAuthentication.bind(this);
-    }
-
-    async componentDidMount() {
-        this.checkAuthentication();
-    }
-
-    async componentDidUpdate() {
-        this.checkAuthentication();
-    }
-	
+export default class Home extends Component {
     render() {
-        if (this.state.authenticated === null)
-            return <React.Fragment></React.Fragment>;
-
         return (
 			<React.Fragment>
-                {this.state.authenticated && this.state.isAdmin && <React.Fragment>
-					<AddNewVote />
-					<Divider hidden />
-				</React.Fragment>}
+                {this.props.showAddVote && (
+					<React.Fragment>
+						<AddNewVote />
+						<Divider hidden />
+					</React.Fragment>
+				)}
 
-				<VoteList isAdmin={this.state.isAdmin} />
+				<VoteList isAdmin={this.props.allowVoteEditing} />
 			</React.Fragment>
 		);
     }
-});
+};
