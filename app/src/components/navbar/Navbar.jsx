f1a3da05b6494c6043cc5582c093c990068e155b
lw <root@fsoc.space>
2019-06-12 14:56:16 +0200

    Restructure Navbar to be more minimal

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -1,54 +1,25 @@
 import React, { Component } from 'react';
-import { withAuth } from '@okta/okta-react';
 
-import { Container, Menu } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 
-import { checkAuthentication } from '../../helpers';
-
-export default withAuth(class Navbar extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { authenticated: null };
-
-        this.login = this.login.bind(this);
-        this.logout = this.logout.bind(this);
-        this.checkAuthentication = checkAuthentication.bind(this);
-    }
-
-    async componentDidMount() {
-        this.checkAuthentication();
-    }
-
-    async componentDidUpdate() {
-        this.checkAuthentication();
-    }
-
-    async login() {
-        this.props.auth.login('/');
-    }
-
-    async logout() {
-        this.props.auth.logout('/');
-	}
-
+export default class Navbar extends Component {
     render() {
+		const { loggedIn } = this.props;
+		const name = loggedIn ? 'logout' : 'login';
+		const handler = loggedIn ? this.props.handleLogin
+			: this.props.handleLogout;
+
         return (
-			<Menu fixed='top' size='large' inverted><Container>
+			<Menu fixed='top' size='large' inverted>
 				<Menu.Item name='votsec' content='votsec' header href="/" />
 
-				{this.state.authenticated === true &&
-					<Menu.Item name='profile' href='/profile' />
-				}
+				{this.props.loggedIn &&
+					<Menu.Item name='profile' href='/profile' />}
 
 				<Menu.Menu position='right'>
-					{this.state.authenticated === true ? (
-						<Menu.Item name='logout' onClick={this.logout} />
-					) : (
-						<Menu.Item name='login' onClick={this.login} />
-					)}
+					<Menu.Item name={name} onClick={handler} />
 				</Menu.Menu>
-			</Container></Menu>
+			</Menu>
         );
     }
-});
+}
