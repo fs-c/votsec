169f9ce14c101b410d483e6f18abb46150815430
lw <root@fsoc.space>
2019-06-16 12:52:25 +0200

    Implement validaion and serialization, closes #28

--- server/src/routes/votes.js
+++ server/src/routes/votes.js
@@ -1,22 +1,74 @@
 const { requireAuth } = require('../auth');
 
 module.exports = (fastify, opts, next) => {
-	fastify.get('/get', async (request, reply) => {
+	const voteProperties = {
+		_id: { type: 'string' },
+		title: { type: 'string' },
+		description: { type: 'string' },
+		startDate: { type: 'number' },
+		endDate: { type: 'number' },
+		hidden: { type: 'boolean' },
+	};
+
+	fastify.addSchema({
+		$id: 'genericError',
+		type: 'object',
+		properties: {
+			error: { type: 'string' },
+			message: { type: 'string' },
+			statusCode: { type: 'number' },
+		}
+	});
+
+	fastify.get('/get', {
+		schema: {
+			query: {
+				id: { type: 'string' },
+				skip: { type: 'number' },
+				limit: { type: 'number' },
+			},
+			response: {
+				'2xx': {
+					type: 'array',
+					items: { type: 'object', properties: voteProperties },
+				},
+				'4xx': 'genericError#',
+			}
+		},
+	}, async (request, reply) => {
 		const { id, skip, limit } = request.query;
 
 		if (id)
 			return await fastify.database.votes.get({ _id: id }, { limit: 1 });
 
 		return await fastify.database.votes.get({ hidden: false }, {
-			skip: parseInt(skip), limit: parseInt(limit)
+			skip, limit
 		}) || [];
 	});
 
 	fastify.post('/add', {
 		preHandler: requireAuth(fastify, 'Admin'),
+		schema: {
+			body: {
+				type: 'object',
+				required: [ 'title', 'description' ],
+				properties: voteProperties,
+			},
+			response: {
+				'2xx': {
+					type: 'object',
+					properties: voteProperties,
+				},
+				'4xx': 'genericError#'
+			},
+		},
 	}, async (request, reply) => {
 		try {
-			if (await fastify.database.votes.get({ title: request.body.title }))
+			const duplicates = await fastify.database.votes.get({
+				title: request.body.title
+			});
+
+			if (duplicates.length > 0)
 				throw new Error('Duplicate title');
 
 			return await fastify.database.votes.add(request.body); 
@@ -27,6 +79,18 @@ module.exports = (fastify, opts, next) => {
 
 	fastify.delete('/delete/:id', {
 		preHandler: requireAuth(fastify, 'Admin'),
+		schema: {
+			params: {
+				type: 'object',
+				properties: {
+					par1: { type: 'string' },
+				},
+			},
+			response: {
+				'2xx': { type: 'object', properties: {} },
+				'4xx': 'genericError#',
+			}
+		},
 	}, async (request, reply) => {
 		fastify.log.info('params: %o', request.params);
 
