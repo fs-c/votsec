0f3dc28dc01e4595131d514e600c4eea11f27e00
lw <root@fsoc.space>
2019-06-23 00:25:00 +0200

    Generalize message

--- app/src/components/ErrorMessage.jsx
+++ app/src/components/ErrorMessage.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import Alert from 'react-bootstrap/Alert';
 
-const ErrorMessage = ({ error }) => {
+const ErrorMessage = ({ prefix = '', error }) => {
 	return (
 		<div className='row justify-content-center align-items-center'>
 			<Alert show={error !== null} variant='danger'
 				className='md-auto'
 			>
-				Failed to load votes: {error}
+				{prefix + error}
 			</Alert>
 		</div>
 	);
