c0fca66a2a83b0b887c989eb46ab6decda0593a3
lw <root@fsoc.space>
2019-06-22 16:12:21 +0200

    Add basic building blocks to home

--- app/src/components/home/Home.jsx
+++ app/src/components/home/Home.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
 import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
-import PopularVotes from '../votes/PopularVotes';
+import VotesGroup from '../votes/VotesGroup';
+import VotesList from '../votes/VotesList';
+
+import withVotes from '../../withVotes';
 
 export default class Home extends Component {
     render() {
+		const PopularVotesGroup = withVotes({ popular: true })(VotesGroup);
+		const FilterableVotesList = withVotes()(VotesList);
+
         return (
 			<React.Fragment>
 				<Container>
-					<PopularVotes />
+					<Jumbotron className='pb-4 pt-4'>
+						<p className='lead'>Popular Votes</p>
+
+						<PopularVotesGroup />
+					</Jumbotron>
+
+					<FilterableVotesList />
 				</Container>
 			</React.Fragment>
 		);
