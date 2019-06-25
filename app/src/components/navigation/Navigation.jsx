8d77e33387244afe165f1ac03faf78d93505c01a
lw <root@fsoc.space>
2019-06-25 22:44:05 +0200

    Implement vote adding, closes #36

--- app/src/components/navigation/Navigation.jsx
+++ app/src/components/navigation/Navigation.jsx
@@ -1,36 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
+import AddVote from '../votes/AddVote';
+
 const Navigation = ({ loggedIn, handleLogin, handleLogout }) => {
 	const handler = loggedIn ? handleLogout : handleLogin;
 
+	const [ addingVote, setAddingVote ] = useState(false);
+
 	return (
-		<Navbar bg='dark' variant='dark' expand='md'>
-			<Container>
-				<Navbar.Brand href='/' className=''>votsec</Navbar.Brand>
+		<>
+			<Navbar bg='dark' variant='dark' expand='md'>
+				<Container>
+					<Navbar.Brand href='/' className=''>votsec</Navbar.Brand>
 
-				<Navbar.Toggle aria-controls='collapsing-navbar' />
+					<Navbar.Toggle aria-controls='collapsing-navbar' />
 
-				<Navbar.Collapse id='collapsing-navbar'>
-					<Nav>
-						{loggedIn &&
-							<Nav.Link href='/profile'>Profile</Nav.Link>}
+					<Navbar.Collapse id='collapsing-navbar'>
+						<Nav>
+							<Nav.Link disabled>About</Nav.Link>
 
-						<Nav.Link disabled>About</Nav.Link>
-					</Nav>
+							{loggedIn && [
+								<Nav.Link key='addVote' onClick={() => setAddingVote(true)}>Add Vote</Nav.Link>,
+								<Nav.Link key='profile' disabled href='/profile'>Profile</Nav.Link>,
+							]}
+						</Nav>
 
-					<Nav className='ml-auto'>
-						<Button variant='outline-light' onClick={handler}>
-							{loggedIn ? 'Logout' : 'Login'}
-						</Button>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
+						<Nav className='ml-auto'>
+							<Button variant='outline-light' onClick={handler}>
+								{loggedIn ? 'Logout' : 'Login'}
+							</Button>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+
+			<AddVote show={addingVote}
+				handleHide={() => setAddingVote(!addingVote)}
+			/>
+		</>
 	);
 };
 
---
+++ app/src/components/votes/AddVote.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useContext } from 'react';
+
+import axios from 'axios';
+
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+import ErrorMessage from '../ErrorMessage';
+
+import { TokenContext } from '../app/App';
+import { buildApiString } from '../../helpers';
+
+const AddVote = ({ show, handleHide }) => {
+	const accessToken = useContext(TokenContext);
+
+	const [ error, setError ] = useState(null);
+	const [ voteTitle, setVoteTitle ] = useState('');
+
+	const handleTitleChange = ({ target }) => {
+		setVoteTitle(target.value);
+	};
+
+	const handleAddVote = async () => {
+		try {
+			await axios.post(buildApiString('votes/add'), {
+				title: voteTitle,
+			}, { headers: { Authorization: `Bearer ${accessToken}` } });
+		} catch (err) {
+			console.log('addVote', err);
+
+			setError('Failed to add vote: ' + err.message);
+		}
+	};
+
+	return (
+		<Modal show={show} onHide={handleHide}>
+			<Modal.Header closeButton>
+            	<Modal.Title>Add Vote</Modal.Title>
+        	</Modal.Header>
+
+			<ErrorMessage error={error} className='pt-3' />
+
+			<Modal.Body>
+				<Form>
+					<Form.Group>
+						<Form.Control type='text' placeholder='Vivamus malesuada rhoncus lacus, convallis lacinia nulla?'
+							value={voteTitle}
+							onChange={handleTitleChange}
+						/>
+
+						<Form.Text className='text-muted'>
+							Your vote title should be as clear, concise and neutral as possible
+						</Form.Text>
+					</Form.Group>
+
+					<Form.Group>
+						<Form.Check disabled custom type='checkbox'>
+							<Form.Check.Label>
+								I agree to the <a href='/tos'>Terms and Conditions</a>
+							</Form.Check.Label>
+						</Form.Check>
+					</Form.Group>
+				</Form>
+			</Modal.Body>
+
+			<Modal.Footer>
+				<Button variant='outline-secondary' onClick={handleHide}>
+					Close
+				</Button>
+				<Button variant='primary' onClick={handleAddVote}>
+					Add Vote
+				</Button>
+        	</Modal.Footer>
+		</Modal>
+	);
+};
+
+export default AddVote;
