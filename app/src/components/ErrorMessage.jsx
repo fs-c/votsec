5fe1fbcf8eb9b10fce0b847292a2e64b5ef2ce83
lw <root@fsoc.space>
2019-06-30 16:23:30 +0200

    Pass className property down to div

--- app/src/components/ErrorMessage.jsx
+++ app/src/components/ErrorMessage.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import Alert from 'react-bootstrap/Alert';
 
-const ErrorMessage = ({ prefix = '', error }) => {
+const ErrorMessage = ({ error, prefix = '', className = '' }) => {
 	return (
-		<div className='row justify-content-center align-items-center'>
+		<div className={`${className} row justify-content-center align-items-center`}>
 			<Alert show={error !== null} variant='danger'
 				className='md-auto'
 			>
