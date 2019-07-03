60a916cf15b38e2f45a7653d7f31937450a27f19
lw <root@fsoc.space>
2019-07-03 21:19:27 +0200

    Make request logging more readable

--- server/src/logging.js
+++ server/src/logging.js
@@ -1,9 +1,8 @@
 const hooks = exports.hooks = {
 	onRequest: async (request, reply) => {
-		request.log.debug(`[${request.id}] >>>>> ${request.req.method} ${request.req.url}`);
+		request.log.debug(`[${request.id}] >>> ${request.req.method} ${request.req.url}`);
 	},
 	onResponse: async (request, reply) => {
-		request.log.debug(`[${request.id}] <${reply.res.statusCode}< ${request.req.method} `
-			+ `${request.req.originalUrl}`);
+		request.log.debug(`[${request.id}] <<< ${request.req.method} ${reply.res.statusCode}`);
 	},
 };
