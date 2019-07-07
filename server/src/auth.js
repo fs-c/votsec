9a65416530278788e8130fbdeb9de6c722653122
lw <root@fsoc.space>
2019-07-07 22:24:17 +0200

    Add basic authorization middleware

---
+++ server/src/auth.js
@@ -0,0 +1,50 @@
+const config = require('../../config');
+
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+
+const { UserError } = require('./error');
+
+const verifier = new OktaJwtVerifier({
+	clientId: config.openID.client,
+	issuer: config.openID.issuer,
+	assertClaims: config.resourceServer.assertClaims,
+});
+
+const extractToken = (headers) => {
+	const match = (headers.authorization || '').match(/Bearer (.+)/);
+
+	if (!match || !match[1]) {
+		throw new UserError('Malformed or missing authentication header', 400);
+	}
+
+	return match[1];
+};
+
+const verifyGroups = async (token, groups = []) => {
+	const claimGroups = token.claims.grp.map((grp) => grp.toLowerCase());
+
+	if (!claimGroups) {
+		throw new UserError('Groups required but not found', 403);
+	}
+
+	for (const group of groups) {
+		if (!claimGroups.includes(group.toLowerCase())) {
+			throw new UserError('Insufficient permissions', 403);
+		}
+	}
+};
+
+const requireAuth = exports.requireAuth = (requiredGroups) => {
+	return async (request, reply) => {
+		const rawToken = extractToken(request.headers);
+		const token = await verifier.verifyAccessToken(rawToken)
+
+		if (requiredGroups) {
+			await verifyGroups(token, requiredGroups);
+		}
+
+		request.user.id = token.claims.uid;
+		request.user.token = token;
+		request.user.rawToken = rawToken;
+	};
+};
