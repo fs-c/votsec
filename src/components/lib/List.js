a4351dd4854069929575cd03e59b432e3d962f49
lw <root@fsoc.space>
2019-09-24 13:02:10 +0200

    Implement list

---
+++ src/components/lib/List.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import cn from 'classnames';
+
+const List = ({ children }) => (
+    <div className='list'>
+        {children}
+
+        <style jsx>{`
+            .list {
+                border-radius: var(--border-radius);
+                border: 1px solid var(--accent-3);
+                /* Since backgrounds would break the border otherwise */
+                overflow: hidden;
+            }
+        `}</style>
+    </div>
+);
+
+const ListItem = ({ children, muted }) => (
+    <div className={cn('list-item', { muted })}>
+        {children}
+
+        <style jsx>{`
+            .list-item:not(:last-child) {
+                border-bottom: 1px solid var(--accent-3);
+            }
+
+            .muted {
+                background-color: var(--accent-1);
+            }
+        `}</style>
+    </div>
+);
+
+List.Item = ListItem;
+
+export default List;
